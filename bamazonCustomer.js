057554689aca7e1019c56fc90a78be4addbf6090
peraltot <peraltot@gmail.com>
2017-12-15 13:28:23 -0500

    Added comments and cleaned up

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,9 +5,9 @@ var colors = require("colors/safe");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  // Your username
+  // username
   user: "root",
-  // Your password
+  // password
   password: "Jake0320",
   database: "bamazon"
 });
@@ -28,16 +28,13 @@ function queryAllProducts() {
     console.log("-----------------------------------");
   });
 }
-
-
-
 // ============================================================
 
 function processOrder() {
-  // query the database for all items being auctioned
+  // query the database for all items sold
   connection.query("SELECT * FROM bamazon.products", function (err, res) {
     if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to bid on
+    // once items displayed, prompt the user for order
     inquirer.prompt([
       {
         name: "choice",
@@ -53,33 +50,29 @@ function processOrder() {
       .then(function (answer) {
 
         var chosenId = answer.choice - 1
-        // var chosenId = answer.choice
         var chosenProduct = res[chosenId]
         var chosenQuantity = answer.quantity
-
+        // check quantity availability
         if (chosenQuantity <= chosenProduct.stock_quantity) {
           var new_stock = parseInt(chosenProduct.stock_quantity) - parseInt(chosenQuantity);
-          // console.log(new_stock);
-          // console.log(chosenId + 1);
-          // console.log("Order placed successfully!");
           console.log(colors.green("The total for " + answer.quantity + " " + chosenProduct.product_name + " is: $" + chosenProduct.price.toFixed(2) * chosenQuantity));
 
           connection.query(
             "UPDATE bamazon.products SET ? WHERE ?",
             [{
-              // stock_quantity: res[chosenId].stock_quantity - parseInt(chosenQuantity)
               stock_quantity: new_stock
             },
             {
               item_id: chosenProduct.item_id
             }
             ], function (error) {
+              // if available order and updated amount above
               if (error) throw err;
               console.log(colors.green("Order placed successfully!"));
               processOrder();
             });
-          // console.log("Items left " + chosenProduct.stock_quantity + " in stock.");
         } else {
+          // if not enough product display amount and start again
           console.log(colors.red("Sorry, insufficient Quanity! There are only " + chosenProduct.stock_quantity + " in stock."));
           processOrder();
         }
